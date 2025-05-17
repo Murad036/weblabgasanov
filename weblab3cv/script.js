6440808f399367d89088712d3a416e570cb45cc4
Murad036 <fhasanov036@gmail.com>
2025-05-17 13:09:25 +0400

    Add files via upload

--- weblab3cv/script.js
+++ weblab3cv/script.js
@@ -1,9 +1,7 @@
-// tema dəyişimi
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
 
-// bölməni göstər/gizlət
 function toggleSection(id) {
   const sec = document.getElementById(id);
   if (sec.style.display === "block") {
@@ -13,7 +11,6 @@ function toggleSection(id) {
   }
 }
 
-// paraqraf əlavə et
 function addContent(id, inputId) {
   const box = document.getElementById(id);
   const inp = document.getElementById(inputId);
@@ -27,7 +24,7 @@ function addContent(id, inputId) {
   }
 }
 
-// siyahı elementini əlavə et
+
 function addListItem(id, inputId) {
   const box = document.getElementById(id);
   const list = box.querySelector("ul");
@@ -42,7 +39,7 @@ function addListItem(id, inputId) {
   }
 }
 
-// saat funksiyası
+
 function updateClock() {
   const now = new Date();
   const h = String(now.getHours()).padStart(2, "0");
@@ -56,6 +53,6 @@ function updateClock() {
   }
 }
 
-// saatı hər saniyə yenilə
+// bura bax bidene saatı hər saniyə yeniləyi bu
 setInterval(updateClock, 1000);
 updateClock();
